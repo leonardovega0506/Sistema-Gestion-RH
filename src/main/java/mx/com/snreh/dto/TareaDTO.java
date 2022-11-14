8c725d0c3f77f5e183e6a09b6580065e9b6dc332
Leonardo Vega <leo.vega0506@gmail.com>
2022-11-14 13:10:44 -0600

    Agregando los primeros DTO

---
+++ src/main/java/mx/com/snreh/dto/TareaDTO.java
@@ -0,0 +1,26 @@
+package mx.com.snreh.dto;
+
+import lombok.Data;
+import mx.com.snreh.model.TrabajadorModel;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.Date;
+
+@Data
+public class TareaDTO {
+
+    private long id_Tarea;
+
+    @NotEmpty(message = "El nombre de la tarea no puede ser vacio")
+    private String nombre;
+
+    @NotEmpty(message = "Debe de tener una Fecha")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE,fallbackPatterns = {"M/d/dd","dd.MM.yyyy"})
+    private Date fecha;
+
+    @NotEmpty(message = "El estado no debe ser vacio o nulo")
+    private String estatus;
+
+
+}
