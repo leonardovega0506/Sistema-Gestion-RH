0d98beb08e25b3ed8747aabd6828cdffd1898902
Leonardo Vega <leo.vega0506@gmail.com>
2022-11-14 13:53:33 -0600

    Agregando DTO

---
+++ src/main/java/mx/com/snreh/exception/ResourceNotFoundException.java
@@ -0,0 +1,21 @@
+package mx.com.snreh.exception;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Data
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+    private static final long serialVersionUID=1L;
+    private String nombreRecurso;
+    private String nombreCampo;
+    private long valorCampo;
+
+    public ResourceNotFoundException( String nombreRecurso, String nombreCampo, long valorCampo) {
+        super(String.format("%s No encontrada con: %s: '%s'",nombreRecurso,nombreCampo,valorCampo));
+        this.nombreRecurso = nombreRecurso;
+        this.nombreCampo = nombreCampo;
+        this.valorCampo = valorCampo;
+    }
+}
