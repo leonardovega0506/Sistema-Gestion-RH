f1a50bdc7e37b5fec85553dd73e127e150f25a99
Leonardo Vega <leo.vega0506@gmail.com>
2022-11-14 17:53:49 -0600

    Agregando el service de GerenteService y GerenteServiceImpl

--- src/main/java/mx/com/snreh/service/implementation/interfaces/TrabajadorService.java
+++ src/main/java/mx/com/snreh/service/implementation/interfaces/TrabajadorService.java
@@ -7,11 +7,11 @@ public interface TrabajadorService {
 
     public TrabajadorDTO crearTrabajador(TrabajadorDTO trabajadorDTO);
 
-    public TrabajadorRespuesta obtenerTrabajadores(int numeroPagina, int sizePagina, String orderBy, String sortDir);
+    public TrabajadorRespuesta obtenerTrabajadores(int numeroPagina, int sizePagina);
 
-    public TrabajadorDTO obtenerTrabajadorByNumeroTrabajador(long numero_trabajador);
+    public TrabajadorDTO obtenerTrabajadorById(long id_trabajador);
 
-    public TrabajadorDTO actualizarTrabajador(TrabajadorDTO trabajadorDTO, long numero_trabajador);
+    public TrabajadorDTO actualizarTrabajador(TrabajadorDTO trabajadorDTO, long id_trabajador);
 
     public void eliminarTrabajador(long numero_trabajador);
 }
