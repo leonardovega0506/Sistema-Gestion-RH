2971ba34afc75770294653a464a7fadb7e6fa13f
Leonardo Vega <leo.vega0506@gmail.com>
2022-11-15 11:59:17 -0600

    Agregar el service de TareaService y TareaServiceImpl

--- src/main/java/mx/com/snreh/repository/ITarea.java
+++ src/main/java/mx/com/snreh/repository/ITarea.java
@@ -6,5 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ITarea extends JpaRepository<TareaModel, Long> {
+<<<<<<< HEAD
     public List<TareaModel> findByTrabajadorModelId(long id_trabajador);
+=======
+    public List<TareaModel> findByTrabajadorId(long id_trabajador);
+>>>>>>> c1f0822 (Agregar el service de TareaService y TareaServiceImpl)
 }
