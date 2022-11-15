4d028cd1354449f74678e8f0572b26ba428d8418
Leonardo Vega <leo.vega0506@gmail.com>
2022-11-15 11:27:49 -0600

    Agregando los Controllers de GerenteController y AdministradorController

--- src/main/java/mx/com/snreh/model/GerenteModel.java
+++ src/main/java/mx/com/snreh/model/GerenteModel.java
@@ -11,10 +11,10 @@ public class GerenteModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id_gerente;
+    private long id_gerente;
 
     @Column(name = "numero_gerente",nullable = false)
-    private int numero_gerente;
+    private long numero_gerente;
 
     @Column(name = "nombre_gerente",nullable = false)
     private String nombre_gerente;
