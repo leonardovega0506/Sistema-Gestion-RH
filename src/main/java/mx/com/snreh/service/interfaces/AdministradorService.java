ff21bc67784d03c9d50ea25909ae122cdc883e82
Leonardo Vega <leo.vega0506@gmail.com>
2022-11-15 11:10:30 -0600

    Agrenado los service de AdministradorService y AdministradorService

---
+++ src/main/java/mx/com/snreh/service/interfaces/AdministradorService.java
@@ -0,0 +1,16 @@
+package mx.com.snreh.service.interfaces;
+
+import mx.com.snreh.dto.AdministradorDTO;
+import mx.com.snreh.respuesta.AdministradorRespuesta;
+
+public interface AdministradorService {
+    public AdministradorDTO crearAdministrador(AdministradorDTO administradorDTO);
+
+    public AdministradorRespuesta obtenerAdministradores(int numeroPagina, int sizePagina);
+
+    public AdministradorDTO obtenerAdministradorById(long id);
+
+    public AdministradorDTO actualizarAdministrador(AdministradorDTO administradorDTO, long id);
+
+    public void eliminiarAdministrador(long id);
+}
