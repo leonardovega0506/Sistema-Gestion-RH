94bf5c842b503a5c0c6479c092d9200cad8290ce
Leonardo Vega <leo.vega0506@gmail.com>
2022-11-16 09:38:49 -0600

    Agregando relaciones de RetardoTrabajadorModel

--- src/main/java/mx/com/snreh/model/RetardoTrabajadorModel.java
+++ src/main/java/mx/com/snreh/model/RetardoTrabajadorModel.java
@@ -13,10 +13,17 @@ public class RetardoTrabajadorModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id_retardo;
 
+    @Column(name = "fecha_retardo",nullable = false)
     private Date fecha_retardo;
 
+    @Column(name = "tiempo_retardo",nullable = false)
     private String tiempo_Retardo;
 
+    @Column(name = "descuento_retardo",nullable = false)
     private double descuento_retardo;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "trabajador_id")
+    private TrabajadorModel trabajadorModel;
 
 }
