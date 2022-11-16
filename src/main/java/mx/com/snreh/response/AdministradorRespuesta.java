ffdacc9a214417f429174b466085d678a2166556
Leonardo Vega <leo.vega0506@gmail.com>
2022-11-16 10:11:41 -0600

    Agregando todos los services

---
+++ src/main/java/mx/com/snreh/response/AdministradorRespuesta.java
@@ -0,0 +1,17 @@
+package mx.com.snreh.response;
+
+import lombok.Data;
+import mx.com.snreh.dto.AdministradorDTO;
+
+import java.util.List;
+
+@Data
+public class AdministradorRespuesta {
+    private List<AdministradorDTO> contenido;
+    private int numeroPagina;
+    private int sizePagina;
+    private long totalElementos;
+    private int totalPaginas;
+    private boolean ultima;
+
+}
