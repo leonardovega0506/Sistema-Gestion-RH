4102ca3f87e954e50c892e9e075e2bb87fd3e695
Leonardo Vega <leo.vega0506@gmail.com>
2022-11-16 09:50:21 -0600

    Agregando DTO e Interface de Retardo

--- src/main/java/mx/com/snreh/repository/IGerente.java
+++
@@ -1,7 +0,0 @@
-package mx.com.snreh.repository;
-
-import mx.com.snreh.model.GerenteModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IGerente extends JpaRepository<GerenteModel, Long> {
-}
