52bfadd42eca83c825cc2586978339dbd6c43e53
Leonardo Vega <leo.vega0506@gmail.com>
2022-11-16 10:20:07 -0600

    Verificacion1.0 Correcta

---
+++ src/main/java/mx/com/snreh/SistemaGestionRhApplication.java
@@ -0,0 +1,13 @@
+package mx.com.snreh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionRhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaGestionRhApplication.class, args);
+    }
+
+}
---
+++ src/main/java/mx/com/snreh/controller/TrabajadorController.java
@@ -0,0 +1,48 @@
+package mx.com.snreh.controller;
+
+import mx.com.snreh.dto.TrabajadorDTO;
+import mx.com.snreh.response.TrabajadorRespuesta;
+import mx.com.snreh.service.implementation.interfaces.TrabajadorService;
+import mx.com.snreh.util.ConstantesGlobales;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("snrhe/trabajadores")
+public class TrabajadorController {
+    @Autowired
+    private TrabajadorService sTrabajador;
+
+    @GetMapping
+    public TrabajadorRespuesta  listarTrabajadores(
+            @RequestParam(value = "pageNo",defaultValue = ConstantesGlobales.NUMERO_PAGINA_DEFECTO,required = false)int numeroPagina,
+            @RequestParam(value = "pageSize",defaultValue = ConstantesGlobales.MEDIDA_PAGINA_DEFECTO, required = false) int sizePagina){
+        return sTrabajador.obtenerTrabajadores(numeroPagina,sizePagina);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TrabajadorDTO> obtenerTrabajadorByNumero_Trabajador(@PathVariable(name = "id") long numero_trabajador){
+        return ResponseEntity.ok(sTrabajador.obtenerTrabajadorById(numero_trabajador));
+    }
+
+    @PostMapping
+    public ResponseEntity<TrabajadorDTO> crearTrabajador(@Valid @RequestBody TrabajadorDTO trabajadorDTO){
+        return new ResponseEntity<>(sTrabajador.crearTrabajador(trabajadorDTO), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TrabajadorDTO> actaulizarTrabajador(@Valid @RequestBody TrabajadorDTO trabajadorDTO,@PathVariable(name = "id")long numero_trabajador){
+        TrabajadorDTO trabajadroRespuesta = sTrabajador.actualizarTrabajador(trabajadorDTO,numero_trabajador);
+        return new ResponseEntity<>(trabajadroRespuesta,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> eliminarTrabajador(@PathVariable(name = "id") long numero_trabajador){
+        sTrabajador.eliminarTrabajador(numero_trabajador);
+        return new ResponseEntity<>("Trabajador eliminado con exito",HttpStatus.OK);
+    }
+}
