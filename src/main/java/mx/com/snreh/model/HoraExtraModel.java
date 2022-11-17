f1f3a1e1af1c5f68e356ca47cc81495d67a1cc5c
leonardovega0506 <leo.vega0506@gmail.com>
2022-11-17 12:58:36 -0600

    Agregando las relaciones de Horas Extra

--- src/main/java/mx/com/snreh/model/HoraExtraModel.java
+++ src/main/java/mx/com/snreh/model/HoraExtraModel.java
@@ -19,4 +19,16 @@ public class HoraExtraModel {
 
     @Column(name = "cantidad_horasExtra",nullable = false)
     private int cantidad_horas;
+
+    @Column(name = "costo_hora",nullable = false)
+    private double costo_hora;
+
+    @Column(name = "aumento_total",nullable = false)
+    private double aumento_total;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_trabajador", nullable = false)
+    private TrabajadorModel trabajadorModel;
+
+
 }
