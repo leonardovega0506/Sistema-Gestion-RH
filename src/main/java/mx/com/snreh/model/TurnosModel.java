13b92d36409a62bf0be3a586908c5ddd8c024f04
leonardovega0506 <leo.vega0506@gmail.com>
2022-11-23 17:39:32 -0600

    Agregando las relaciones entre Turno y Trabajador

--- src/main/java/mx/com/snreh/model/TurnosModel.java
+++ src/main/java/mx/com/snreh/model/TurnosModel.java
@@ -1,8 +1,11 @@
 package mx.com.snreh.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Data
@@ -12,6 +15,7 @@ public class TurnosModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_turno")
     private int id_turno;
 
     @Column(name = "turno",nullable = false)
@@ -19,5 +23,11 @@ public class TurnosModel {
 
     @Column(name = "cantidad_horas",nullable = false)
     private int cantidad_horas;
+
+    @ManyToMany
+    @JoinTable(name = "turnos_trabajadores",joinColumns = @JoinColumn(name = "id_turno", referencedColumnName = "id_turno"),inverseJoinColumns = @JoinColumn(name = "id_trabajador",referencedColumnName = "id_trabajador"))
+    @JsonBackReference
+    private Set<TrabajadorModel> trabajadoresTurnos = new HashSet<>();
+
 
 }
