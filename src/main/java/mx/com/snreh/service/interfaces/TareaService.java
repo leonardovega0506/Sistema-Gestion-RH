92d598317989f5b0f4d13d77caaf7cd576ae1f3f
Leonardo Vega <leo.vega0506@gmail.com>
2022-11-24 17:41:19 -0600

    Agregando más metodos a Service de Tarea

--- src/main/java/mx/com/snreh/service/interfaces/TareaService.java
+++ src/main/java/mx/com/snreh/service/interfaces/TareaService.java
@@ -5,11 +5,13 @@ import mx.com.snreh.dto.TareaDTO;
 import java.util.List;
 
 public interface TareaService {
-    public TareaDTO crearTarea (long id_trabajador, TareaDTO tareaDTO);
+    public TareaDTO createTarea (long id_trabajador, TareaDTO tareaDTO);
 
-    public List<TareaDTO> obtenerTareasTrabajador(long id_trabjador);
+    public List<TareaDTO> findTareasTrabajador(long id_trabjador);
 
-    public TareaDTO obtenerTareaByID(long id_trabajador, long id_tarea);
+    public TareaDTO findTareaByID(long id_trabajador, long id_tarea);
 
-    public TareaDTO actualizarTarea(long id_trabajador, long id_tarea, TareaDTO tareaDTO);
+    public TareaDTO updateTarea(long id_trabajador, long id_tarea, TareaDTO tareaDTO);
+
+    public List<TareaDTO> findAllTareas();
 }
